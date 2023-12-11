3c03995213481140a5facc3c3497d7529ff5835b
?? <91481987+4SV322@users.noreply.github.com>
2023-12-11 18:39:16 +0800

    smth not ended
    Do i really need it?

---
+++ app/Http/Controllers/Campus/Model/CampusUserData.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Http\Controllers\Campus\model;
+
+class CampusUserData
+{
+    /**
+     * фамилия
+     * @var string
+     */
+    public string $last_name;
+
+    /**
+     * имя
+     * @var string
+     */
+    public string $name;
+    /**
+     * отчество
+     * @var string
+     */
+    public string $second_name;
+    /**
+     * гендерный признак { М || Ж || пустая строка || FALSE }
+     * @var string
+     */
+    public string $gender;
+    /**
+     * ссылка на фотографию { ссылка || пустая строка }
+     * @var string
+     */
+    public string $photo;
+    /**
+     * верифицированный адрес электронной почты
+     * @var string
+     */
+    public string $email;
+    /**
+     * признак принадлежности к группе "студенты" { 0 || 1 }
+     * @var int
+     */
+    public int $is_student;
+    /**
+     * признак принадлежности к группе "преподаватели" { 0 || 1 }
+     * @var int
+     */
+    public int $is_teacher;
+    /**
+     * массив данных по пользователю группы "студент" { массив || пустой массив || FALSE }
+     * @var object
+     */
+    public CampusStudentData | null $data_student;
+    /**
+     * массив данных по пользователю группы "преподаватель" { массив || пустой массив || FALSE }
+     * @var object
+     */
+    public CampusTeacherData | null $data_teacher;
+    /**
+     * массив данных по пользователю группы "руководитель проекта" { массив || пустой массив || FALSE }
+     * @var object
+     */
+    public CampusDirectorData | null $data_director;
+    /**
+     * массив данных по пользователю группы "Соискатель" { массив || пустой массив || FALSE }
+     * @var object
+     */
+    public CampusApplicantData | null $data_applicant;
+    /**
+     * массив данных по пользователю группы "Администратор" { массив || пустой массив || FALSE }
+     * @var object
+     */
+    public CampusAdminData | null $data_admin;
+    /**
+     * массив значений идентификатора пользователя в АИС "УНИВЕРСИТЕТ" { массив || пустой массив || FALSE }
+     * @var array
+     */
+    public array $mira_id;
+
+    public function __construct(string $last_name, string $name, string $second_name, string $gender, string $photo, string $email, int $is_student, int $is_teacher, CampusStudentData | null $data_student, CampusTeacherData | null $data_teacher, array $mira_id)
+    {
+        $this->$last_name = $last_name;
+        $this->$name = $name;
+        $this->$second_name = $second_name;
+        $this->$gender = $gender;
+        $this->$photo = $photo;
+        $this->$email = $email;
+        $this->$is_student = $is_student;
+        $this->$is_teacher = $is_teacher;
+        $this->$is_applicant = $is_applicant;
+        $this->$is_director = $is_director;
+        $this->$is_admin = $is_admin;
+        $this->$data_student = $data_student;
+        $this->$data_teacher = $data_teacher;
+        $this->$data_director = $data_director;
+        $this->$data_applicant = $data_applicant;
+        $this->$data_admin = $data_admin;
+        $this->$mira_id = $mira_id;
+    }
+}
